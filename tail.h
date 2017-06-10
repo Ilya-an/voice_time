7ee4b62a6d4625f6043081956229173ae3eaa52c
Илья <ilya@localhost.localdomain>
2017-06-10 17:17:39 +0300

    First modularity

---
+++ tail.h
@@ -0,0 +1,7 @@
+#ifndef TAIL_H_INCLUDED
+#define TAIL_H_INCLUDED
+
+bool find_tail (char *line, char *tail);
+char* replace_tail (char *line, char *tail);
+
+#endif // TAIL_INCLUDED
--- voice_time.h
+++ voice_time.h
@@ -1,9 +1,9 @@
 #ifndef VOICE_TIME_H_INCLUDED
 #define VOICE_TIME_H_INCLUDED
 
+//#include "tail.c"
+
 char* transl_number (char* str_num);
-bool find_tail (char *line, char *tail);
-char* replace_tail (char *line, char *tail);
 char* type_hour (char* str_hour, char* hour);
 char* type_min (char* str_min, char* minuts);
 void settime (struct tm *u, struct tm *y);
